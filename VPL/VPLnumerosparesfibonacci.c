fe3dfe7a4d6ceb61859c234b3ddff938e7919fa0
Jorge Duarte <migueljunior1000@gmail.com>
2021-11-16 19:05:45 -0300

    vpl

--- VPL/VPLnumerosparesfibonacci.c
+++ VPL/VPLnumerosparesfibonacci.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int X, f1 = 0, f2 = 1, f3, soma = 0;
+    long long int X, f1 = 0, f2 = 1, f3 = 0, soma = 0;
 
-    scanf("%d", &X);
+    scanf("%lld", &X);
     
 
-    while (f2 < X)
+    while (f2 <= X)
     {
-        f3 = f2 + f1;
-        f1 = f2;
-        f2 = f3;
-        
-        if (f3 % 2 == 0)
-        {
+        if (f3 % 2 == 0){
             soma = soma + f3;
         }
         
+        f3 = f2 + f1;
+        f1 = f2;
+        f2 = f3;
     }
-    printf("%d \n", soma);
+    
+    printf("%lld \n", soma);
 
     return 0;
 }
